881b06be78319ba0746d9babca7bd518c534f113
Tatanagasai20 <142307963+Tatanagasai20@users.noreply.github.com>
2025-06-27 15:38:29 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,10 +10,3 @@ public class DemoApplication {
     }
 }
 
-@RestController
-class HelloController {
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, Dockerized Spring Boot!";
-    }
-}
